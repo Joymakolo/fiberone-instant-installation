eaaf6d931a386bcd7fc0d3bacac500f5fc622689
Joy <jaymakolo3@gmail.com>
2024-12-31 06:10:03 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function requestInstantInstallation() {
+    // Hide the button and show the form
+    document.getElementById('installButton').style.display = 'none';
+    document.getElementById('installationForm').style.display = 'block';
+}
